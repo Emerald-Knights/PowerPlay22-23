f57a3b5f748f32288740b2a23e50c89da4a62a55
Shining <shinypup06@gmail.com>
2022-10-15 21:59:12 -0400

    changed some formatting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DetectorPipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DetectorPipeline.java
@@ -19,10 +19,6 @@ public class DetectorPipeline extends OpenCvPipeline {
 
     double pctColorError = 0.2;
 
-    //the amount of pixels detected of each color
-    public static int p1 = 0;
-    public static int g2 = 0;
-    public static int o3 = 0;
 
     //the final value after comparison for which sleeve color is shown
     public static int sleeveColor;
@@ -44,51 +40,47 @@ public class DetectorPipeline extends OpenCvPipeline {
     public Mat processFrame(Mat input){
         Size imageSize = input.size();
         Mat output = input.clone();
-
-        o3 = 0;
-        p1 = 0;
-        g2 = 0;
-
+        int orangeCnt = 0;
+        int greenCnt = 0;
+        int purpleCnt = 0;
         Rect crosshair1 = new Rect(new Point(imageSize.width/2 + xDev + scanWidth, (imageSize.height)/2 + yDev - scanHeight), new Point((int)(imageSize.width/2) + xDev - scanWidth, imageSize.height/2 +yDev + scanHeight));
         Imgproc.rectangle(output, crosshair1, new Scalar(4,233,78),3,8);
         for(int i = (int)(imageSize.height)/2 + yDev - scanHeight; i < imageSize.height/2 +yDev + scanHeight ; i++){
             for(int j = (int)(imageSize.width/2) + xDev - scanWidth; j < imageSize.width/2 + xDev + scanWidth; j++)
             {
-
                 pixelColor = input.get(i,j);
-
                 //orange
                 if (pixelColor[0] > 210 + brightness){
-                    o3++;
+                    orangeCnt++;
                     double[] newColor = {250, 0, 0, pixelColor[3]};
                     output.put(i, j, newColor);
                 }
                 //green
                 if (pixelColor[1] > 160 + brightness && pixelColor[0] < 180 + brightness){
-                    g2++;
-                    double[] newColor1 = {0, 250, 0, pixelColor[3]};
-                    output.put(i, j, newColor1);
+                    greenCnt++;
+                    double[] newColor = {0, 250, 0, pixelColor[3]};
+                    output.put(i, j, newColor);
                 }
                 //purple
                 if (pixelColor[2] > 120 + brightness && pixelColor[1] < 130){
-                    p1++;
-                    double[] newColor2 = {0, 0, 250, pixelColor[3]};
-                    output.put(i, j, newColor2);
+                    purpleCnt++;
+                    double[] newColor = {0, 0, 250, pixelColor[3]};
+                    output.put(i, j, newColor);
                 }
                 //if it is white
                 if (pixelColor[0] > 210 + brightness && pixelColor[1] > 210 + brightness && pixelColor[2] > 210 + brightness){
-                    double[] newColor3 = {0, 0, 0, pixelColor[3]};
-                    output.put(i, j, newColor3);
+                    double[] newColor = {0, 0, 0, pixelColor[3]};
+                    output.put(i, j, newColor);
                 }
             }
         }
-        if(o3>p1 && o3 >g2){
+        if(orangeCnt > purpleCnt && orangeCnt > greenCnt){
             sleeveColor = 3;
         }
-        if(p1>o3 && p1 > g2){
+        if(purpleCnt > orangeCnt && purpleCnt > greenCnt){
             sleeveColor = 1;
         }
-        if(g2>o3 && g2>p1){
+        if(greenCnt > orangeCnt && greenCnt > purpleCnt){
             sleeveColor = 2;
         }
 
